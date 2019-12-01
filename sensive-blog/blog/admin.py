34bea78ee35cccc7d34741b518c232c9b5dbe605
TheEgid <40724661+TheEgid@users.noreply.github.com>
2019-12-01 19:23:37 +0300

    Fix admin pages load time

--- sensive-blog/blog/admin.py
+++ sensive-blog/blog/admin.py
@@ -1,7 +1,46 @@
-from django.contrib import admin
 from blog.models import Post, Tag, Comment
+from django.contrib import admin
 
 
-admin.site.register(Post)
-admin.site.register(Tag)
-admin.site.register(Comment)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('published_at',)
+
+    list_display = ('title',
+                    'slug',
+                    'image',
+                    'author',
+                    'published_at',
+                    )
+
+    raw_id_fields = ('likes', 'author', 'tags',)
+
+    class Meta:
+        ordering = ['-published_at']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('published_at',)
+
+    list_display = ('published_at',
+                    'author',
+                    'text',
+                    'post',
+                    )
+
+    raw_id_fields = ('author', 'post',)
+
+    class Meta:
+        ordering = ['published_at']
+
+
+@admin.register(Tag)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('title', )
+
+    class Meta:
+        ordering = ["title"]
